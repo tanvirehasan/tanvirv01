c3a1485f43cb3dd997bd7893bb8966cf0c9f80e0
tanvirehasan <tanvirhasanbcse@gmail.com>
2020-09-22 06:36:49 +0600

    laravel

---
+++ resources/views/fontend/index.blade.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>TANVIR | Web Developer & Graphic Designer in Bangladesh</title>
+	<meta name="description" content="Free Web tutorials">
+	<meta name="keywords" content="HTML, CSS, JavaScript">
+	<meta name="author" content="Md Tanvir Hasan">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="https://mdtanvirhasan.com/ims/ico.png" type="image/gif" sizes="16x16">
+
+	<link rel="stylesheet" type="text/css" href="{{url('public/fontend/css/main.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{url('public/fontend/css/mobilefrnd.css')}}">
+	<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://kit.fontawesome.com/14fb7079b2.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<header>
+		<nav><!--slider-->
+			<div class="container">
+				<div class="navbar-brand"><a href="index.php"><img src="https://mdtanvirhasan.com/ims/l1.png" alt="logo" ></a></div>
+				<div class="mnav"><i class="fas fa-bars"></i></div>
+				<div class="manNav">	
+					<ul>
+						<li><a href="">HOME</a></li>
+						<li><a href="#aboutme">ABOUT ME</a></li>
+						<li><a href="">RESUME</a></li>
+						<li><a href="">PORTFOLIO</a></li>
+						<li><a href="">SERVICES</a></li>
+						<li><a href="">BLOG</a></li>
+						<li><a href="">CLIENTS</a></li>
+						<li><a href="">CONTACT</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="slider"><!--slider-->
+			<img src="https://www.elegantthemes.com/blog/wp-content/uploads/2017/08/featuredimage-7.jpg">			
+		</div>
+	</header>
+
+<div class="row">
+	<div class="container">
+		<div class="title"><h1>About Me!</h1></div>
+			<div class="aboutme">
+				<div class="aboutext">
+					<p><em>I, Md. Tanvir Hasan am a promising young champ in the digital platform.</em></p>
+					<p>Having a glorious experience of five years in web and graphics designing made me a suitable choice for anyone to hire in the respective fields. As a CSE graduate I started my career as a data analyst at an early age of 19 and crawled my way up to become a successful web designer.I got the opportunity to work as a creative executive for event management firm Hello Events.</p>
+					<p>I continued to be a freelancer in my leisure and have completed projects successfully for different local and international companies. After serving in Hello events for a year I, then joined as Web Developer in Bangladesh’s one of the leading consultancy firms FutureLeaders. I got promoted to be the creative designer of FutureLeaders. I have participated in training on Web Designing arranged by ICT Divison of BD government. I also recently completed a training on Digital Marketing by Google. I also have skills on basic android app development, professional video editing,Youtube, SEO Digital Marketing and Google Adsense. I aspire to become a leading web developer of the country. I am also a proud member of Bangladesh Organization of Learning and Development (BOLD) for last 2 years.</p>
+				</div>
+				<div class="aboutimage"></div>
+			</div>		
+	</div>
+</div>
+
+<div class="row" style="background: #f0f0f0;"><!--whatcanido-->
+	<div class="container" id="whatcanido">
+		<div class="title"><h1>WHAT CAN I DO</h1></div>
+
+			<div class="whatido">
+				<div class="whatidobox">
+					<div class="service webdev">
+						<i class="fas fa-laptop-code"></i>
+						<h2>Web<br>Development</h2>
+					</div>
+					<div class="service graphic">
+						<i class="fa fa-paint-brush"></i>
+						<h2>Creative<br>Design</h2>
+					</div>
+					<div class="service degital">
+						<i class="fas fa-bullhorn"></i>
+						<h2>Digital<br>Marketing</h2>
+					</div>
+				</div>
+				<div class="spese" style="margin-top: 40px;"></div>
+				<div class="whatidobox">
+					<p>PHP</p>
+					<div class="skills">
+						<div class="php"></div>
+						<div class="persent">78%</div>
+					</div>
+					<p>Wordpress</p>
+					<div class="skills">
+						<div class="Wordpress"></div>
+						<div class="persent">85%</div>
+					</div>
+					<p>HTML & CSS</p>
+					<div class="skills">
+						<div class="htmlcss"></div>
+						<div class="persent">91%</div>
+					</div>
+					<p>Adobe illustrator</p>
+					<div class="skills">
+						<div class="illustrator"></div>
+						<div class="persent">70%</div>
+					</div>
+					<p>Adobe premiere pro</p>
+					<div class="skills">
+						<div class="premiere"></div>
+						<div class="persent">85%</div>
+					</div>
+				</div>
+			</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="container">
+		<div class="title"><h1>SERVICES</h1></div>
+
+		<div class="services">
+
+			<div class="servicebox" >
+				<i class="fas fa-laptop-code"></i>
+				<h2>Web Development</h2>
+				<p>Our web design and development team have the experience to make your project a success</p>
+			</div>
+
+			<div class="servicebox" >
+				<i class="fas fa-laptop-code"></i>
+				<h2>Digital Marketing</h2>
+				<p>Facebook, Youtube, Google, SEO, Email and affiliate are the most powerfull tools of digital marketing.</p>
+			</div>
+
+			<div class="servicebox" >
+				<i class="fas fa-laptop-code"></i>
+				<h2>Graphic Design</h2>
+				<p>We offer clean, clear and unique print designs with your company’s image in mind.</p>
+			</div>
+
+			<div class="servicebox" >
+				<i class="fas fa-laptop-code"></i>
+				<h2>APP DEVELOPMENT</h2>
+				<p>we understand the significance of quality mobile application development</p>
+			</div>
+
+			<div class="servicebox" >
+				<i class="fas fa-laptop-code"></i>
+				<h2>SEO</h2>
+				<p>We offer a range of options to improve the efficiency of serving up your webpages.</p>
+
+			</div>
+
+			<div class="servicebox" >
+				<i class="fas fa-laptop-code"></i>
+				<h2>Video Editing</h2>
+				<p>High quality Production and Video Editing Service at very affordable and cheap price.</p>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
+
